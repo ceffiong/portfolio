5f9126e3c74a2d3144a7eee3cad290e0490b13a8
effiongcharles <talkwithcharles@gmail.com>
2023-10-24 20:27:27 +0200

    some ui updates

--- js/script.js
+++ js/script.js
@@ -4,24 +4,48 @@ AOS.init({
   mirror: true,
 });
 
-// Mobile navigation function
+//scrolling
+let mybutton = document.getElementById("btn-top");
+let myNav = document.getElementById("mobile-nav");
 
+let homePage = document.getElementById("home");
+let portfolioPage = document.getElementById("portfolio");
+let blogPage = document.getElementById("blog");
+let connectPage = document.getElementById("connect");
+let mobileNav = document.getElementById("mobile-nav");
+
+document.body.addEventListener(
+  "click",
+  (e) => {
+    if (e.target !== mobileNav && !mobileNav.contains(e.target)) {
+      closeNav();
+    }
+  },
+  true
+);
+
+// Mobile navigation function
 function openNav() {
-  document.getElementById("mobile-nav").style.width = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
-  getElementById("contact").style.pointerEvents = "none";
+  mybutton.style.display = "none";
+  myNav.style.width = "250px";
+  homePage.classList.add("no-event");
+  portfolioPage.classList.add("no-event");
+  blogPage.classList.add("no-event");
+  connectPage.classList.add("no-event");
+  document.body.classList.add("body-no-scroll");
 }
 
 function closeNav() {
-  document.getElementById("mobile-nav").style.width = "0";
-  document.body.style.backgroundColor = "white";
+  myNav.style.width = "0";
+  homePage.classList.remove("no-event");
+  portfolioPage.classList.remove("no-event");
+  blogPage.classList.remove("no-event");
+  connectPage.classList.remove("no-event");
+  document.body.classList.remove("body-no-scroll");
 }
 
 var prevScrollPos =
   document.documentElement.scrollTop || document.body.scrollTop;
-
-//scrolling
-let mybutton = document.getElementById("btn-top");
 
 //show or hide "scroll to top button"
 function scrollFunction() {
